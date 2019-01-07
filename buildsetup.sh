9acc0647352de0eaf5a229bd67e6cc47b3878a6c
Vishal Mittal <imvishalmittal@gmail.com>
2019-01-07 13:42:13 -0800

    Updated gpm.yml

--- buildsetup.sh
+++ buildsetup.sh
@@ -1,5 +1,5 @@
 yes|sudo yum install wget
 yes|sudo yum install lsof
 
-gpm install PolicyCenter:%env.PC_Build_Number%
-gpm install OptionsPack -enable_asynchronous_quoting=yes
+gpm install -f gpm.yml
+
