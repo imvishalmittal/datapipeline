2107c86070ae7a77a7ccf5b4ad227e02ca7e0b4c
Vishal Mittal <imvishalmittal@gmail.com>
2019-01-07 17:28:18 -0800

    modified shell script

--- buildsetup.sh
+++ buildsetup.sh
@@ -1,5 +1,16 @@
 yes|sudo yum install wget
 yes|sudo yum install lsof
 
+echo "Installing build with Kafka Plugin enabled"
 gpm install -f gpm.yml
 
+echo "Run Server"
+./gwb runServer
+
+sleep 400
+
+httpStatus=$(curl -IL --silent http://localhost:8180/pc | grep HTTP)
+ echo $httpStatus
+ if [[ $httpStatus = *"200"* ]] then
+    echo "server started"
+ fi
